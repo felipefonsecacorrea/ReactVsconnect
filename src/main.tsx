d23c27df7bd8b4dc65f131b74b2fdfb8388ca302
Felipe Fonseca Correa <felipefcfcfc@gmail.com>
2023-08-16 21:55:16 -0300

    Atualiza lista dev , api e servicos

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Footer from './components/Footer'
+import Header from './components/Header'
 // import App from './App'
-import './index.css'
-import Home from './pages/Home'
-import ListaServicos from './pages/ListaServicos'
+import './index.css';
+import ListaServicos from './pages/ListaServicos';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import ListaDevs from './pages/ListaDevs';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ListaServicos />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='ListaServicos' element={<ListaServicos/>} />
+        <Route path='ListaDevs' element={<ListaDevs/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+
+  </React.StrictMode>
 )
